f423f4ed00aa11a73958b7d365db49c7b3e9e96f
emesefedev <emesefedev@gmail.com>
2025-03-26 21:46:10 +0100

    feat: server works

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
-var http = require('http')
+const http = require('http')
+const fs = require('fs/promises')
+const url = require('url')
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World!')
-}).listen(8080);
+const supportedUrls = [
+  '/',
+  '/index.html',
+  '/about',
+  '/contact-me'
+]
+
+
+async function respondHtml(res, { file, html, status = 200 }) {
+  if (html == null && file != null) {
+    html = await fs.readFile(file)
+  }
+
+  res.writeHead(status, { 'content-type': 'text/html' })
+  res.write(html)
+  res.end()
+}
+
+
+function handleErrors(server) {
+
+  return async (req, res) => {
+    try {
+      await server(req, res)
+    } catch (error) {
+      console.error("Unhandled error", error)
+      res.writeHead(500, { 'content-type': 'text/plain' })
+      res.write('Internal Server Error')
+      res.end()
+    }
+  }
+}
+
+function respond404(res) {
+  return respondHtml(res, {
+    file: './404.html',
+    status: 404
+  })
+}
+
+function redirect(res, url) {
+  if(!url) {
+    throw new Error('url parameter missing')
+  }
+  res.writeHead(302, 'Found', {
+    'location': url
+  })
+  res.end()
+}
+
+
+http
+  .createServer(
+    handleErrors(async function (req, res) {
+      const reqUrl = url.parse(req.url, true);
+      console.log(reqUrl.pathname, req.url)
+      if (!supportedUrls.includes(reqUrl.pathname)) {
+        return await respond404(res)
+      }
+      const fileName = reqUrl.pathname === "/"
+        ? './index.html'
+        : '.' + reqUrl.pathname + '.html'
+
+      return respondHtml(res, {
+        file: fileName
+      })
+    })
+  )
+  .listen(8080);
